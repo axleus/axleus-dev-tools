08a095ceaa9a5c35f527cf485dd6f003ab5ab26b
Joey Smith <jsmith@webinertia.net>
2025-05-04 17:37:31 -0500

    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- src/TimerTrait.php
+++ src/TimerTrait.php
@@ -12,9 +12,9 @@ trait TimerTrait
 
     public static function timer(string $marker): void
     {
-        static $time = [];
-        $now   = hrtime(true);
-        $delta = isset($time[$marker]) ? $now - $time[$marker] : 0;
+        static $time   = [];
+        $now           = hrtime(true);
+        $delta         = isset($time[$marker]) ? $now - $time[$marker] : 0;
         $time[$marker] = $now;
         $elapsed = $delta / 1e+6;
         if (isset($time[$marker]) && $elapsed > 0) {
