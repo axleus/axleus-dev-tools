9877e32b175d77569335e8bbc4ab8738ad255ac1
Joey Smith <jsmith@webinertia.net>
2025-05-04 17:35:49 -0500

    Remove unused code. Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- src/TimerTrait.php
+++ src/TimerTrait.php
@@ -6,31 +6,9 @@ namespace Axleus\DevTools;
 
 use Tracy\Debugger;
 
-use function number_format;
-
 trait TimerTrait
 {
-    private const TOTAL_RUNTIME = 'total-runtime';
-
-    private ?string $marker;
-    /**
-     *
-     * @param null|string $marker defaults to 'total-runtime'
-     * @param null|string $tag Is appended to the passed $marker
-     * @return void
-     */
-    public function stopWatch(string $marker, ?string $tag = '')
-    {
-        if (null !== $marker) {
-            $this->marker = $marker;
-        }
-        if (!empty($tag)) {
-            $tag = '.' . $tag;
-        }
-        $marker = $this->marker === $marker ? $this->marker : static::TOTAL_RUNTIME;
-        $time = number_format(Debugger::timer($marker) * 1000, 5, '.', "\u{202f}") . ' ms';
-        Debugger::barDump($time, $marker . $tag);
-    }
+    private string $marker;
 
     public static function timer(string $marker): void
     {
